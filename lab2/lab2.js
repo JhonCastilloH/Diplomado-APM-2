9965e9f2bfa38978fffb0fad910928ec8f4fb263
JhonCastilloH <profe.johncastillo@gmail.com>
2015-03-28 12:35:58 -0500

    proyecto 3
    javaScript

---
+++ lab2/lab2.js
@@ -0,0 +1,13 @@
+var array= [1,11, 'aa','a'];
+console.log(array);
+console.log(typeof(array));
+array.push('b');
+console.log(array.sort());
+
+tasks = ['comer', 'dormir', 'jugar'];
+
+console.log(tasks);
+
+tasks.splice(1,3, "Estudiar", "saltar", "correr");
+
+console.log(tasks);
