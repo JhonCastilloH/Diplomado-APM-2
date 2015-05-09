75e4e4a7d5c627b39d81e2f09774b92ecda4fd43
JhonCastilloH <profe.johncastillo@gmail.com>
2015-05-08 21:53:50 -0500

    ajuste

--- lab31/gulpfile.js
+++ lab31/gulpfile.js
@@ -1,58 +1,16 @@
-var express = require('express'),
-	app     = express(),
-	http    = require('http'),
-	server  = http.createServer(app),
-	io      = require('socket.io')(server), //corre sobre mi servidor
-	port    = process.env.PORT || 8585;
+/*
+* Dependencias
+*/                                  //modulos para ser instalados
+var gulp = require('gulp'),
+	concat = require('gulp-concat'), //mofdulo para concatenar
+	uglify = require('gulp-uglify');  //modulopara crear .min
 
-server.listen(port, function(){
-
-	console.log('Listen %d', port); //arranca servidor
-});
-
-app.use(express.static(__dirname)); //subiemdo staticos
-
-// Ruta(s) que se manejaran
-app.get('/', function(req, res){     //ruta donde estara el chat
-	res.sendfile(__dirname + '/index.html');
-});
-
-
-// Variable `usernames` para los usuarios conectados
-var usernames = {};
-//con el metodo on comienzo a escuchar los eventos.
-io.sockets.on('connection', function(socket){ //connection es un evento web socket el primero que debe ocurrir 1
-
-	// Cuando el cliente, browser, emite el evento `sendchat` este debe escuchar y ejecutar
-	socket.on('sendchat', function(data){  //sendchat es un evento el segundo del servidor al usuario 2
-		io.sockets.emit('updatechat', socket.username, data); //emit es para emitir al cliente con el evento udatechat 3
-	});
-
-	// Cuando el cliente emite un evento `adduser` , este escucha y ejecuta
-	socket.on('adduser', function(username){ //evento aduser 4
-
-		// Almacenamiento del nombre de usuario en la sesión que toma info del cliente
-		socket.username = username;
-
-		// Adicionando el usuario `socket.username` al objeto `usernames`
-		usernames[username] = username;
-
-		// enviando a los clientes conectados el objet con la lista de usuarios del objeto `usernames`
-		socket.broadcast.emit('updatechat', 'SERVIDOR', username + 'Esta conectado');
-		//con broadcast envio a todos que entro un usuario.
-		// enviamos petición al mismo cliente que solicita
-		socket.emit('updatechat', 'SERVIDOR', 'Usted esta conectado');
-					//nombre metodo, nombre de envio, informacion de envio
-		// Actualizamos la lista de usuarios en el chat
-		io.sockets.emit('updateusers', usernames);
-	});
-
-	socket.on('disconnect', function(){
-
-		delete usernames[socket.username];
-
-		io.sockets.emit('updateusers', usernames);
-
-		socket.broadcast.emit('updatechat', 'SERVIDOR', socket.username + 'Esta desconectado');
-	});
+/*
+* Configuración de la tarea 'demo'
+*/
+gulp.task('concat-files', function(){
+	gulp.src('source/*.js')    //toma los nombres de los js en la pcarpeta source
+		.pipe(concat('lab31.min.js'))  //concatena los nombre con la palabra min
+		.pipe(uglify())     // crea los .min
+		.pipe(gulp.dest('build/')) //guarda los .min en la carpeta build
 });
